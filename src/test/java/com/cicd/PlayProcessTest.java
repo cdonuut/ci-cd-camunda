1a6d37633924dd06f447ea0432acc2590c762108
cdonnut <donat.mehmeti@camunda.com>
2025-04-22 22:49:39 +0200

    Clean push

---
+++ src/test/java/com/cicd/PlayProcessTest.java
@@ -0,0 +1,33 @@
+package com.cicd;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.DeploymentEvent;
+import io.camunda.zeebe.process.test.api.ZeebeTestEngine;
+import io.camunda.zeebe.process.test.assertions.BpmnAssert;
+import io.camunda.zeebe.process.test.extension.ZeebeProcessTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ZeebeProcessTest
+public class PlayProcessTest {
+
+  private ZeebeClient client;
+  private ZeebeTestEngine engine;
+
+  @Test
+  void shouldDeployProcess() {
+    DeploymentEvent deploymentEvent = client
+            .newDeployResourceCommand()
+            .addResourceFromClasspath("processes/PlayProcess.bpmn")
+            .send()
+            .join();
+
+    assertTrue(deploymentEvent.getKey() > 0, "Deployment key should be greater than 0");
+    assertEquals(1, deploymentEvent.getProcesses().size(), "Should deploy exactly one process");
+
+    BpmnAssert.assertThat(deploymentEvent)
+            .containsProcessesByBpmnProcessId("play_process");
+  }
+}
